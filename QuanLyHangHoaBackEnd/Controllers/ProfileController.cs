15954748d0618dbfd5e5c78a84c09b490f11f17c
tunglongvuong1102 <caoviettung1102@gmail.com>
2024-08-07 22:37:36 +0700

    Add files via upload

---
+++ QuanLyHangHoaBackEnd/Controllers/ProfileController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyHangHoaEsteel_BackEnd.Models;
+using QuanLyHangHoaEsteel_BackEnd.Repository;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace QuanLyHangHoaEsteel_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/profile")]
+    public class ProfileController : Controller
+    {
+        private readonly IProfileRepository profileRepository;
+
+        public ProfileController(IProfileRepository profile)
+        {
+            this.profileRepository = profile;
+        }
+        [HttpGet]
+        [Route("get-by-username")]
+        public IActionResult GetByUserName(string userName)
+        {
+            try
+            {
+                var prof = profileRepository.GetByAccount(userName);
+                return Ok(new
+                {
+                    response_code = 200,
+                    response_data = prof
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    response_code = 300,
+                    response_data = new Profile(),
+                });
+            }  
+        }
+        [HttpPost]
+        [Route("edit-profile")]
+        public IActionResult AddProfile(string userName, Profile profile)
+        {
+            var checkExists = profileRepository.GetByAccount(userName);
+            if(checkExists == null)
+            {
+                var add = profileRepository.AddProfile(profile, userName);
+                if (!add)
+                {
+                    return Ok(new
+                    {
+                        response_code = 300,
+                        response_data = "Lỗi khi thêm hồ sơ"
+                    });
+                }
+            }
+            else
+            {
+                var update = profileRepository.UpdateProfile(profile);
+                if (!update)
+                {
+                    return Ok(new
+                    {
+                        response_code = 300,
+                        response_data = "Lỗi khi thêm hồ sơ"
+                    });
+                }
+            }
+            
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = "Sửa hồ sơ thành công"
+            });
+        }
+        [HttpDelete]
+        [Route("delete-profile")]
+        public IActionResult DeleteProfile(string userName)
+        {
+            var prof = profileRepository.GetByAccount(userName);
+            var del = profileRepository.DeleteProfile((int)prof.Id);
+            if (!del)
+            {
+                return Ok(new
+                {
+                    response_code = 300,
+                    response_data = "Lỗi khi xóa hồ sơ"
+                });
+            }
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = "Xóa hồ sơ thành công"
+            });
+        }
+    }
+}
---
+++ QuanLyHangHoaBackEnd/Controllers/QRController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using QuanLyHangHoaEsteel_BackEnd.Models;
+using QuanLyHangHoaEsteel_BackEnd.Repository;
+using QRCoder;
+using System.Drawing;
+
+namespace QuanLyHangHoaEsteel_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/qrcode")]
+    public class QRController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public QRController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+       
+        [HttpDelete("delete-qr")]
+        public IActionResult Delete(string productCode)
+        {
+            // qRCodeRepository.DeleteQRCode(qr.Id);
+            var prod = productRepository.GetById(productCode);
+            prod.hasCode = 0;
+            var del = productRepository.UpdateProduct(prod);
+            if (!del)
+            {
+                return Ok(new
+                {
+                    response_code = 00,
+                    response_data = "Lỗi khi xóa mã"
+                });
+            }
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = "Xóa mã thành công"
+            });
+        }
+        [HttpGet("save-by-prod")]
+        public IActionResult SaveByProd(string ProductCode)
+        {
+            var prod = productRepository.GetById(ProductCode);
+            try
+            {
+                byte[] content = new byte[0];
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(prod.ProductCode, QRCodeGenerator.ECCLevel.M);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                string filepath = "D:\\MediaDatabase\\QuanLySanPhamEsteel\\QRImage\\qr_" + prod.ProductCode + ".png";
+                if (System.IO.File.Exists(filepath))
+                {
+                    System.IO.File.Delete(filepath);
+                }
+                using (FileStream fs = new FileStream(filepath, FileMode.Create))
+                {
+                    qrCodeImage.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                }
+                content = System.IO.File.ReadAllBytes(filepath);
+                var temp = new
+                {
+                    name = prod.ProductName,
+                    data = content.ToArray()
+                };
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = temp
+            });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    response_code = 300,
+                    response_data = "Lỗi khi xử lý mã"
+                });
+            }
+        }
+        [HttpGet("save-all")]
+        public IActionResult SaveAllQr()
+        {
+            var prod = productRepository.GetAllCoded();
+            List<dynamic> imageData = new List<dynamic>();
+            foreach(var i in prod)
+            {
+                try
+                {
+                    byte[] content = new byte[0];
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(i.ProductCode, QRCodeGenerator.ECCLevel.M);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                    string filepath = "D:\\MediaDatabase\\QuanLySanPhamEsteel\\QRImage\\qr_" + i.ProductCode + ".png";
+                    if(System.IO.File.Exists(filepath))
+                    {
+                        System.IO.File.Delete(filepath);
+                    }
+                    using(FileStream fs = new FileStream(filepath, FileMode.Create))
+                    {
+                        qrCodeImage.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                    content = System.IO.File.ReadAllBytes(filepath);
+                    var temp = new
+                    {
+                        name = i.ProductName,
+                        data = content.ToArray()
+                    };
+                    imageData.Add(temp);
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new
+                    {
+                        response_code = 300,
+                        response_data = "Lỗi khi xử lý mã"
+                    });
+                }
+               
+            }
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = imageData
+            });
+        }
+    }
+}
