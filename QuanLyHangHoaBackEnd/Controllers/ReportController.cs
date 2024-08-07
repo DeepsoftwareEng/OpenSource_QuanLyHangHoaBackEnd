393287f349ab9feb938b4abc2d79ed67fe658c04
tunglongvuong1102 <caoviettung1102@gmail.com>
2024-08-07 22:35:32 +0700

    Add files via upload

---
+++ QuanLyHangHoaBackEnd/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyHangHoaEsteel_BackEnd.Models;
+using QuanLyHangHoaEsteel_BackEnd.Repository;
+
+namespace QuanLyHangHoaEsteel_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/report")]
+    public class ReportController : Controller
+    {
+        private readonly IReportRepository reportRepository;
+
+        public ReportController(IReportRepository reportRepository)
+        {
+            this.reportRepository = reportRepository;
+        }
+        [HttpGet("product")]
+        public IActionResult ReportProduct()
+        {
+            var rs = reportRepository.ExportExcelProd();
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = rs,
+                name = DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_BaoCaoHangHoa.xlsx"
+            });
+        }
+        [HttpPost("in-out-log")]
+        public IActionResult ReportLog(ProductLogfilter filter)
+        {
+            var rs = reportRepository.ExportExcelInOut(filter);
+            return Ok(new
+            {
+                response_code = 200,
+                response_data = rs,
+                name = DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + "_BaoCaoNhapXuat.xlsx"
+            });
+        }
+    }
+}
